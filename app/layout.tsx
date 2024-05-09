9c89347b921d8314f7b863a1eb502b123571598d
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-05-10 01:33:24 +0530

    kela welaaa

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const IMB_Plex = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -20,10 +21,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <ClerkProvider>
+      <html lang="en">
       <body className={cn("font-IMB_Plex antialiased", IMB_Plex.variable)}>
         {children}
       </body>
     </html>
+    </ClerkProvider>
+    
   );
 }
